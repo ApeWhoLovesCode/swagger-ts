23562418c79d4a3e0808a0877d84982f032525f2
linhuanheng <linhuanheng@vzan.com>
2025-02-11 16:12:57 +0800

    swagger-ts版本升级

--- swagger-typescript-api/index.js
+++ swagger-typescript-api/index.js
@@ -1,18 +1,18 @@
-const { generateApi } = require('swagger-typescript-api');
-const path = require('path');
-const outputDir = path.resolve(__dirname, "../src/s-t-api")
+const { generateApi } = require("swagger-typescript-api");
+const path = require("path");
+const outputDir = path.resolve(__dirname, "../src/s-t-api");
 
-/** 
+/**
  * npx swagger-typescript-api -p ./assets/api.json -o ./src/s-t-api -n myApi.ts
  */
 
 /* NOTE: all fields are optional expect one of `output`, `url`, `spec` */
 generateApi({
-  input: path.resolve(__dirname, "../assets/api.json"),
+  input: path.resolve(__dirname, "../assets/swagger.json"),
   // url: 'https://dev-api.lightwork.com.cn/api-doc/#/',
   output: outputDir,
   // modular: true,
-  templates: path.resolve(__dirname, './templates/api.eta'),
+  templates: path.resolve(__dirname, "./templates/api.eta"),
   // axios: false,
   // routeTypes: true,
 });
